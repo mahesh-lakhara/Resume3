c3d6d13e56fb85039456968ed087ca6ab012dbe4
backbenchrs <meetlenwala912@gmail.com>
2023-03-07 12:03:21 -0800

    commit4

--- app/src/main/java/com/mmm/resume3/MainActivity2.java
+++ app/src/main/java/com/mmm/resume3/MainActivity2.java
@@ -3,6 +3,7 @@ package com.mmm.resume3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,10 +15,16 @@ public class MainActivity2 extends AppCompatActivity {
 
     EditText edtName,edtAddress,edtEmail,edtmoblie;
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        preferences = getSharedPreferences("Data",0);
+        editor = preferences.edit();
 
         txtStart2 = findViewById(R.id.txtStart2);
         edtName = findViewById(R.id.edtName);
@@ -29,9 +36,15 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = edtName.getText().toString();
-                String address = edtName.getText().toString();
-                String emai = edtName.getText().toString();
-                String mobli = edtName.getText().toString();
+                String address = edtAddress.getText().toString();
+                String emai = edtEmail.getText().toString();
+                String mobli = edtmoblie.getText().toString();
+
+                editor.putString("name",name);
+                editor.putString("address",address);
+                editor.putString("email",emai);
+                editor.putString("moblie",mobli);
+                editor.commit();
 
                 if (name.isEmpty()) {
                     edtName.setError("enter Name !");
@@ -45,6 +58,7 @@ public class MainActivity2 extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
                     startActivity(intent);
                 }
+
             }
         });
 
