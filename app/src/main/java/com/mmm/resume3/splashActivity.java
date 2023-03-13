ab0e2f12c851f6641987684b689cfa5be3c8a9a3
monul <monu.lakhara2004@gmail.com>
2023-03-13 22:14:35 +0530

    commit_data6

--- app/src/main/java/com/mmm/resume3/splashActivity.java
+++ app/src/main/java/com/mmm/resume3/splashActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splashActivity extends AppCompatActivity {
+
+    ImageView logo;
+    TextView txtrrr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+        txtrrr = findViewById(R.id.txtrrr);
+
+        Animation move = AnimationUtils.loadAnimation(splashActivity.this,R.anim.move);
+        logo.startAnimation(move);
+        txtrrr.startAnimation(move);
 
         new Handler().postDelayed(new Runnable() {
             @Override
